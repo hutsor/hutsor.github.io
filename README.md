a75878c427786de6c2a95190c1d265ff930f4d96
olimot <olimot@gmail.com>
2024-05-11 10:33:55 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# 헛솔 템플릿
+
+사용한 라이브러리:
+
+- typescript
+- eslint
+- prettier
+- react
+- clsx
+- esbuild
+- gh-pages
---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="main">
+      <h1>HUTSOR</h1>
+      <menu className="main-menu">
+        <li className="main-menu__item">
+          <a href="https://hutsor.github.io/hutsori">헛소리 제조기</a>
+        </li>
+      </menu>
+    </div>
+  );
+}
